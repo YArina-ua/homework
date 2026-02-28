ac42ebbc3a7cd0921c61ef16dc3eaf52da0d82a6
YArina-ua <153173144+YArina-ua@users.noreply.github.com>
2026-02-28 19:38:03 +0200

    Add files via upload

---
+++ homework_19/js/script.js
@@ -0,0 +1,11 @@
+// burger-menu
+"use strict";
+document.addEventListener("click", documentActions);
+
+function documentActions(e) {
+  const targetElement = e.target;
+
+  if (targetElement.closest(".header__icon-menu")) {
+    document.documentElement.toggleAttribute("data-menu-open");
+  }
+}
